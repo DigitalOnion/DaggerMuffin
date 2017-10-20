0ce960363e87f18e14430f7f7ec5622a6a7751f0
DigitalOnion <luis.viruena@gmail.com>
2017-10-19 23:47:59 -0700

    Add Navigation between MainActivity and SecondActivity and slight IO change

--- app/src/main/java/com/outerspace/daggermuffin/SecondActivity.java
+++ app/src/main/java/com/outerspace/daggermuffin/SecondActivity.java
@@ -2,6 +2,7 @@ package com.outerspace.daggermuffin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,4 +11,9 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
     }
+
+    public void onClickMainActivity(View view) {
+        super.onBackPressed();
+    }
+    
 }
---
+++ app/src/main/java/com/outerspace/daggermuffin/model/MuffinPojo.java
@@ -0,0 +1,11 @@
+package com.outerspace.daggermuffin.model;
+
+import javax.inject.Inject;
+
+public class MuffinPojo {
+
+    @Inject
+    public MuffinPojo() { }
+
+    public String muffinFlavor;
+}
